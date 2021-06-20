71dcd23486c88142be3ea5c92a6a052257100978
gildaok <gilda@oktana.io>
2021-06-20 14:04:10 -0400

    Hotfixes and testclass added

--- force-app/main/default/lwc/productBackup/productBackup.js
+++ force-app/main/default/lwc/productBackup/productBackup.js
@@ -1,18 +1,28 @@
 import { LightningElement, wire, api } from 'lwc';
 import getProductId from "@salesforce/apex/ProductBackupController.getProductId";
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import IS_ACTIVE from '@salesforce/schema/Product2.IsActive';
 
 export default class ProductBackup extends LightningElement {
 
     @api recordId;
     productId;
 
-    @wire(getProductId, { productBackupId: "$recordId"})
+    @wire(getProductId, { productBackupId: "$recordId" })
     wiredResult(result) {
         if (result.data) {
-          this.productId = result.data;
-        } else if(result.error){
+            this.productId = result.data;
+        } else if (result.error) {
             console.error(result.error);
         }
-      }
-    
+    }
+
+    @wire(getRecord, { recordId: "$productId", fields: [IS_ACTIVE] })
+    product2;
+
+    get isActive(){
+        let isActive = getFieldValue(this.product2.data, IS_ACTIVE);
+        return isActive ? 'Yes' : 'No';
+    }
+
 }
